e2453d993bc031d2afc1cd03170f43d8cdfc4d90
five1star <five1star0420@gmail.com>
2023-07-05 11:19:01 +0900

    firebase setup

--- App.tsx
+++ App.tsx
@@ -1,5 +1,9 @@
 import { StatusBar } from "react-native";
 import Navigation from "./components/Navigate/Navigation";
+import { useEffect } from "react";
+import { firebase } from "@react-native-firebase/database";
+
+import { db } from "./firebaseConfig";
 
 export default function App() {
   return <Navigation />;
--- views/ScreenA/index.tsx
+++ views/ScreenA/index.tsx
@@ -1,9 +1,25 @@
 import { StyleSheet, Text, View } from "react-native";
 
+import { db } from "../../firebaseConfig";
+import { useEffect, useState } from "react";
+import { collection, getDocs, setDoc } from "firebase/firestore";
+
 export default function ScreenA() {
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    getDocs(collection(db, "test"))
+      .then((snapshot) =>
+        snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+      )
+      .then((data: any) => {
+        if (data.length) setName(data[0]?.name);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>캬캬</Text>
+      <Text>{name}</Text>
     </View>
   );
 }
